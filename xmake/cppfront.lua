bbdad3fc8863b6785c71fce649bbf50d01400b3d
Arthur LAURENT <arthur.laurent4@gmail.com>
2022-09-23 18:10:53 +0200

    Improve cppfront rule for msvc

--- xmake/cppfront.lua
+++ xmake/cppfront.lua
@@ -23,10 +23,7 @@ rule("cppfront")
                 local cpp2file_copied = path.join(outputdir, path.filename(cpp2file))
                 local cppfile = cpp2file_copied:sub(1, -2)
 
-                if not os.exists(cppfile) then
-                    os.touch(cppfile)
-                end
-                target:add("files", cppfile)
+                -- target:add("files", cppfile, {always_added = true})
             end
         end
     end)
@@ -59,29 +56,46 @@ rule("cppfront")
 
         opt.rootjob = batchjobs:group_leave() or opt.rootjob
         batchjobs:group_enter(target:name() .. "/build_c++2", {rootjob = opt.rootjob})
+        local compinst = target:compiler("cxx")
         for _, cpp2file in ipairs(sourcebatch.sourcefiles) do
             local cpp2file_copied = path.join(outputdir, path.filename(cpp2file))
             local cppfile = cpp2file_copied:sub(1, -2)
 
             batchjobs:addjob(cpp2file, function(index, total)
+                local objectfile = target:objectfile(cppfile)
+
                 depend.on_changed(function()
                     local dependfile = target:dependfile(cpp2file)
                     local dependinfo = depend.load(dependfile) or { files = {cpp2file }}
-                    if depend.is_changed(dependinfo, {lastmtime = os.mtime(dependfile), files = {cpp2file}}) then
-                        print("AAAAAAAAAAAAAAAAAAAAAA", os.mtime(cpp2file))
-                        os.cp(cpp2file, outputdir)
+                    os.cp(cpp2file, outputdir)
 
-                        progress.show((index * 100) / total, "${color.build.object}compiling.cpp2 %s", cpp2file)
+                    progress.show((index * 100) / total, "${color.build.object}compiling.cpp2 %s", cpp2file)
 
-                        local flags = { cpp2file_copied }
+                    local flags = { cpp2file_copied }
 
-                        local _, err = os.iorunv("cppfront", table.join(flags, common_flags))
-                        assert(err == "", err)
+                    local _, err = os.iorunv("cppfront", table.join(flags, common_flags))
+                    assert(err == "", err)
+
+                    progress.show((index * 100) / total, "${color.build.object}compiling.cpp %s", cpp2file)
+
+                    compinst:compile(cppfile)                    local compinst = target:compiler("cxx")
+
+                    if not os.isdir(path.directory(objectfile)) then
+                        os.mkdir(path.directory(objectfile))
+                    end
+                    if is_plat("windows") then
+                        local msvc = target:toolchain("msvc")
+                        os.vrunv(compinst:program(), winos.cmdargv(table.join(compinst:compflags({target = target}), {"/Fo" .. objectfile, cppfile})), {envs = msvc:runenvs()})
+                    else
+                        os.vrunv(compinst:program(), winos.cmdargv(table.join(compinst:compflags({target = target}))))
                     end
 
                     depend.save(dependinfo, dependfile)
 
-                end, {dependfile = target:dependfile(cppfile), files = {cpp2file, cppfile}})
+                end, {dependfile = target:dependfile(cpp2file), files = {cpp2file, cppfile}})
+
+                sourcebatch.objectfiles = sourcebatch.objectfiles or {}
+                table.insert(sourcebatch.objectfiles, target:objectfile(cppfile))
             end, { rootjob = opt.rootjob })
         end
     end, {batch = true})
